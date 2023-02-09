f1ed56add8446fe4d1ce705b3e4c28fc1d883373
maarioherns21 <maarioherns21@gmail.com>
2023-02-09 12:57:56 +0800

    added validation for input length keyword and  snippet

--- backend/copykitt_api.py
+++ backend/copykitt_api.py
@@ -1,23 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from copykitt import genererate_branding_snippet, genererate_keywords
 app = FastAPI()
 
 
+MAX_INPUT_LENGTH = 32
+
 @app.get("/generate_snippet")
 async def genererate_snippet_api(prompt: str):
+    validate_input_length(prompt)
     snippet = genererate_branding_snippet(prompt)
-    return {"snippet": snippet ,"keywords": []}
+    return {"snippet": snippet, "keywords": []}
 
     # uvicorn copykitt_api:app --reload
 
+
 @app.get("/generate_keyword")
 async def genererate_keyword_api(prompt: str):
+    validate_input_length(prompt)
     keywords = genererate_keywords(prompt)
-    return {"snippet": None ,"keywords": keywords}
+    return {"snippet": None, "keywords": keywords}
 
 
 @app.get("/genererate_snippets_and_keyword")
 async def genererate_snippets_and_keyword_api(prompt: str):
+    validate_input_length(prompt)
     snippet = genererate_branding_snippet(prompt)
     keywords = genererate_keywords(prompt)
-    return {"snippet": snippet ,"keywords": keywords}
+    return {"snippet": snippet, "keywords": keywords}
+
+#added validation fro characther only allowed for keyword search
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400, detail=f"Input length is to long. Must be under {MAX_INPUT_LENGTH} Characters")
